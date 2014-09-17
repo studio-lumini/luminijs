27054e9e4bb7d81808dd4121771b0e26fafc282d
arnaudlumini <arnaud@lumini.fr>
2014-09-17 11:35:45 +0200

    Refactoring.

---
+++ build/build.bash
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+cd ../bin
+
+if [[ -f lumini.js ]]
+then
+	rm lumini.js
+fi
+touch lumini.js
+
+cd ../src
+
+cat 'google/GATracker.class.js' >> ../bin/lumini.js
+#echo -e '\n' >> ../bin/lumini.js
+cat 'google/Stats.class.js' >> ../bin/lumini.js
+
+cat 'mvc/AbstractObject.class.js' >> ../bin/lumini.js
+
+cat 'mvc/stage/StageSizeEvent.class.js' >> ../bin/lumini.js
+cat 'mvc/stage/StageSizeModel.class.js' >> ../bin/lumini.js
+cat 'mvc/stage/StageSizeController.class.js' >> ../bin/lumini.js
+
+cat 'mvc/AbstractView.class.js' >> ../bin/lumini.js
+
+cat 'mvc/selection/AbstractSelectionContentView.class.js' >> ../bin/lumini.js
+
+cat 'utils/Cookie.class.js' >> ../bin/lumini.js
+cat 'utils/UrlToolbox.class.js' >> ../bin/lumini.js
+cat 'utils/social/SocialSharer.class.js' >> ../bin/lumini.js
+
+cat 'mvc/code/CodeEvent.class.js' >> ../bin/lumini.js
+cat 'mvc/code/CodeModel.class.js' >> ../bin/lumini.js
+cat 'mvc/code/CodeController.class.js' >> ../bin/lumini.js
+cat 'mvc/code/FlushingCodeController.class.js' >> ../bin/lumini.js
+cat 'mvc/code/KeyboardCodeController.class.js' >> ../bin/lumini.js
+
+cat 'mvc/selection/SelectionEvent.class.js' >> ../bin/lumini.js
+cat 'mvc/selection/SelectionModel.class.js' >> ../bin/lumini.js
+cat 'mvc/selection/SelectionController.class.js' >> ../bin/lumini.js
+
+cat 'mvc/facebook/FacebookModel.class.js' >> ../bin/lumini.js
+cat 'mvc/facebook/FacebookController.class.js' >> ../bin/lumini.js
+cat 'mvc/facebook/FacebookEvent.class.js' >> ../bin/lumini.js
+
+cat 'mvc/twitter/TwitterEvent.class.js' >> ../bin/lumini.js
+cat 'mvc/twitter/TwitterModel.class.js' >> ../bin/lumini.js
+cat 'mvc/twitter/TwitterController.class.js' >> ../bin/lumini.js
+
+cat 'mvc/selection/SWFAddressSelectionController.class.js' >> ../bin/lumini.js
+cat 'mvc/selection/SWFAddressSelectionEvent.class.js' >> ../bin/lumini.js
+cat 'mvc/selection/SWFAddressSelectionModel.class.js' >> ../bin/lumini.js
+
+cat 'mvc/menu/AbstractMenuItemView.class.js' >> ../bin/lumini.js
+
+cat 'utils/AnimationHelper.class.js' >> ../bin/lumini.js
+cat 'utils/drupal/DrupalServices.class.js' >> ../bin/lumini.js
