9de88e099e881e950aa89e7d2584d6e0aaa65c5a
arnaudlumini <arnaud@lumini.fr>
2015-09-03 18:04:55 +0200

    Fix.

--- src/mvc/selection/page/swfaddress/SWFAddressSelectionController.class.js
+++ src/mvc/selection/page/swfaddress/SWFAddressSelectionController.class.js
@@ -1,52 +1,51 @@
-function SWFAddressSelectionController(){
-	this.started = false;
-	this.nextvalue = undefined;
+function SWFAddressSelectionController() {
+  this.started = false;
+  this.nextvalue = undefined;
 }
 
 SWFAddressSelectionController._extends(PageSelectionController);
 
 //known bug: this function must be called early, before SWFAddressEvent.INIT is dispatched.
-SWFAddressSelectionController.prototype.init = function()
-{
-    SWFAddressSelectionController._super.init.call(this);
-	if(typeof SWFAddress != "undefined"){
-		jQuery(SWFAddress).bind(SWFAddressEvent.INIT, jQuery.proxy(this.onSWFAddressInit, this));
-	}else{
-		this.started = true;
-	}
+SWFAddressSelectionController.prototype.init = function () {
+  SWFAddressSelectionController._super.init.call(this);
+  if (typeof SWFAddress != "undefined") {
+    jQuery(SWFAddress).bind(SWFAddressEvent.INIT, jQuery.proxy(this.onSWFAddressInit, this));
+  }
+  else {
+    this.started = true;
+  }
 };
 
-SWFAddressSelectionController.prototype.setCurrent = function(current)
-{
-	if(this.started){
-		if(typeof SWFAddress != "undefined"){
-			SWFAddress.setValue(current);
-		}else{
-			this.loadPath(current);
-		}
-	}else{
-		this.nextvalue = current;
-	}
+SWFAddressSelectionController.prototype.setCurrent = function (current) {
+  if (this.started) {
+    if (typeof SWFAddress != "undefined") {
+      SWFAddress.setValue(current);
+    }
+    else {
+      this.loadPage(current);
+    }
+  }
+  else {
+    this.nextvalue = current;
+  }
 };
 
 //private
-SWFAddressSelectionController.prototype.onSWFAddressInit = function(event)
-{
-	this.started = true;
-	jQuery(SWFAddress).bind(SWFAddressEvent.CHANGE, jQuery.proxy(this.onSWFAddressChange, this));
-	if(this.nextvalue != undefined){
-		this.setCurrent(this.nextvalue);
-		this.nextvalue = '';
-	}
+SWFAddressSelectionController.prototype.onSWFAddressInit = function (event) {
+  this.started = true;
+  jQuery(SWFAddress).bind(SWFAddressEvent.CHANGE, jQuery.proxy(this.onSWFAddressChange, this));
+  if (this.nextvalue != undefined) {
+    this.setCurrent(this.nextvalue);
+    this.nextvalue = '';
+  }
 };
 
-SWFAddressSelectionController.prototype.onSWFAddressChange = function(event)
-{
-	this.model.history.push(SWFAddress.getValue());
-    this.model.setCurrent(SWFAddress.getValue());
+SWFAddressSelectionController.prototype.onSWFAddressChange = function (event) {
+  this.model.history.push(SWFAddress.getValue());
+  this.model.setCurrent(SWFAddress.getValue());
 
-	this.setState(SWFAddressSelectionEvent.LOADING);
-	
-	this.loadPage(SWFAddress.getValue().substr(1));
-	return false;
+  this.setState(PageSelectionEvent.LOADING);
+
+  this.loadPage(SWFAddress.getValue().substr(1));
+  return false;
 };
